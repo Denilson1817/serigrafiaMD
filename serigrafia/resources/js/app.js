30327376fc135510bd5de06fd2834528901dc90e
Yielcy <jonthan.galio108@gmail.com>
2021-09-22 18:28:43 -0500

    Actualización

--- serigrafia/resources/js/app.js
+++ serigrafia/resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+function crearNuevoCatalogo(){
+    document.getElementById("crearNuevoC").style.display="block";
+}
+
+function cerrarImgNueCat(){
+    document.getElementById("crearNuevoC").style.display="none";
+}
