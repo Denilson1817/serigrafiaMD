304380d932bde2f4ba0be46e62a7b596e20ca187
angel.rogu017@gmail.com <angel.rogu017@gmail.com>
2021-09-28 01:40:23 -0500

    .

--- serigrafia/resources/views/admin/deleteCatalogo.blade.php
+++ serigrafia/resources/views/admin/deleteCatalogo.blade.php
@@ -29,5 +29,23 @@
     </div>
 <!--</form>-->
 
+<form action="{{route('catalog.update')}}" method="post">
+    @csrf
+    <div class="grid grid-cols-6 gap-4">
+        <div class="col-span-6">
+            <h3 class="w-full text-xl">¿Estas seguro de eliminar este diseño?</h3>
+        </div>
+        <input type="hidden" name="id_catalog" id="id_catalog" value="{{$catalog->id}}">
+        <div class="col-span-6 md:col-span-3">
+            <label for="nombre">Nombre del Diseño </label>
+            <label value="{{$catalog->Nombre}}"> </label>
+            <!--<input type="text" name="nombre" id="nombre" class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" value="{{$catalog->Nombre}}">-->
+        </div>
+        <div class="col-span-6">
+            <input type="submit" value="ENVIAR" class="crear_cata_enviar">
+        </div>
+    </div>
+</form>
+
 @endforeach
 @endsection
